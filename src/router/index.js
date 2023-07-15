50412a97cd405289291ef939c7fd8763e9b86798
Adiansyah <mail.adiansyah@gmail.com>
2023-07-15 20:03:57 +0700

    fix: .env file location move to root

--- src/router/index.js
+++ src/router/index.js
@@ -8,11 +8,11 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from) => {
-    if (to.path === '/tasks') {
-        return {
-            name: 'login'
-        }
-    }
+    // if (to.path === '/tasks') {
+    //     return {
+    //         name: 'login'
+    //     }
+    // }
 })
 
 export default router;
